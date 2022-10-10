ccba16dd3f12557925b4b782401c85f37d59da05
Alexis Franco <ronaldofranco196@gmail.com>
2022-10-09 21:54:32 -0300

    ejercicios seccion 5 numbers

---
+++ numbers;.js
@@ -0,0 +1,14 @@
+let alturaCm = 171;
+
+let alturaMts = 1.71;
+
+let kilos = 58.9;
+
+let alturaMetros = Math.ceil(alturaMts);
+console.log(alturaMetros);
+
+let pesoBajo = Math.floor(kilos);
+console.log(pesoBajo);
+
+let max = (Number.MAX_VALUE) === (Number.MAX_VALUE + 1);
+console.log(max);
